671a8c6b4f787c2fa2db2640e8da7211cad6ed00
Lucia Harcegova <fakeluciamail@gmail.com>
2024-03-11 14:46:40 +0100

    Template 1

---
+++ minimal.rb
@@ -0,0 +1,43 @@
+# restart all running spring processes on MacOs
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# Gemfile
+# All env gems
+general_gems = <<~RUBY
+  gem 'autoprefixer-rails'
+  gem 'simple_form', github: 'heartcombo/simple_form'
+  gem 'sassc-rails'
+RUBY
+
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
+  general_gems
+end
+
+# Development and Test gems
+inject_into_file 'Gemfile', after: 'group :development, :test do' do
+  <<~RUBY
+    # Store secret keys in .env file
+    gem 'dotenv-rails'
+    # Check performance of queries [https://github.com/kirillshevch/query_track]
+    gem 'query_track'
+  RUBY
+end
+
+# Setting up rspec
+inject_into_file 'Gemfile', after: 'group :test do' do
+  <<~RUBY
+    gem 'rspec-rails'
+    gem 'factory_bot_rails'
+    gem 'faker'
+    gem "selenium-webdriver"
+    gem "webdrivers"
+    gem "axe-core-capybara"
+    gem "axe-core-rspec"
+  RUBY
+end
+
+## After bundle
+
+after_bundle do
+  generate('rspec:install')
+end
