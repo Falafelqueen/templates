e85037ce197fddcab4d1780fe67023dd403eb202
Lucia Harcegova <fakeluciamail@gmail.com>
2024-03-11 15:31:18 +0100

    Remove new line

--- minimal.rb
+++ minimal.rb
@@ -29,17 +29,17 @@ development_gems = <<~RUBY
   gem 'query_track'
 RUBY
 
-inject_into_file 'Gemfile', before: 'group :development, :test do\n' do
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
   general_gems
 end
 
 # Development and Test gems
-inject_into_file 'Gemfile', after: 'group :development, :test do\n' do
+inject_into_file 'Gemfile', after: 'group :development, :test do' do
   development_gems
 end
 
 # Test gems
-inject_into_file 'Gemfile', after: 'group :test do\n' do
+inject_into_file 'Gemfile', after: 'group :test do' do
   test_gems
 end
 
