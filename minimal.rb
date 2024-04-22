e109b145abc94e55149cf3d0566fe2833c0ded17
Lucia Harcegova <fakeluciamail@gmail.com>
2024-04-22 11:11:30 +0200

    Fix bug in generators.rb

--- minimal.rb
+++ minimal.rb
@@ -161,14 +161,15 @@ end
 # Do not generate extra files
 initializer 'generators.rb', <<-CODE
   Rails.application.config.generators do |g|
-  g.test_framework :rspec,
-                  fixtures: false,
-                  fixture_replacement :factory_bot,
-                  dir: "spec/factories",
-                  view_specs: false,
-                  helper_specs: false,
-                  routing_specs: false,
-                  request_specs: false,
-                  controller_specs: false
+    g.test_framework :rspec, {
+                     fixtures: false,
+                     fixture_replacement: :factory_bot,
+                     dir: 'spec/factories',
+                     view_specs: false,
+                     helper_specs: false,
+                     routing_specs: false,
+                     request_specs: false,
+                     controller_specs: false
+                    }
   end
 CODE
