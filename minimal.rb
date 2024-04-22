574b7b97bbeb8f3969312dfd9a9fa476b00e9eda
Lucia Harcegova <fakeluciamail@gmail.com>
2024-04-22 11:10:28 +0200

    change in generators.rb

--- minimal.rb
+++ minimal.rb
@@ -162,12 +162,13 @@ end
 initializer 'generators.rb', <<-CODE
   Rails.application.config.generators do |g|
   g.test_framework :rspec,
-  fixtures: false,
-  fixture_replacement :factory_bot, dir: "spec/factories",
-  view_specs: false,
-  helper_specs: false,
-  routing_specs: false,
-  request_specs: false,
-  controller_specs: false
+                  fixtures: false,
+                  fixture_replacement :factory_bot,
+                  dir: "spec/factories",
+                  view_specs: false,
+                  helper_specs: false,
+                  routing_specs: false,
+                  request_specs: false,
+                  controller_specs: false
   end
 CODE
